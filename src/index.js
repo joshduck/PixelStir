f2a040caafd2683dc2c9cefbbfa5fa4358417104
Josh Duck <github@joshduck.com>
2017-11-28 20:43:07 +1000

    Travis

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-/*require("./ui");
-require("./image-loader");
-require("./image-renderer");
-require("./file-uploader");
-require("./drag-handler");
-require("./ui/base-screen");
-require("./ui/login-screen");
-require("./ui/photo-viewer");
-require("./ui/photo-drawer");
-require("./ui/photo-thumb");
-require("./ui/base-editor");
-require("./ui/crop-editor");
-require("./ui/filter-editor");
-require("./ui/preview-editor");
-require("./data/photo-collection");
-require("./data/photo-model");
-require("./data/crop-model");*/
-
 const Core = require("./core");
 const instance = new Core();
 
